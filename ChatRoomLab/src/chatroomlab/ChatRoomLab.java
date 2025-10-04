fb77e54b4b03df8c95639fda450450e6a16dea03
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-10-04 15:11:03 +0530

    Initial Commit

---
+++ ChatRoomLab/src/chatroomlab/ChatRoomLab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatroomlab;
+
+/**
+ *
+ * @author hiranyamendis
+ */
+public class ChatRoomLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Chat Room Lab");
+    }
+    
+}
