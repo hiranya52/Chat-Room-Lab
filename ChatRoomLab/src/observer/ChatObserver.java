84bf5c73018240197802cb4fc3e2fd3e805ee929
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-10-05 15:19:51 +0530

    Add observer

---
+++ ChatRoomLab/src/observer/ChatObserver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package observer;
+
+/**
+ *
+ * @author hiranyamendis
+ */
+public interface ChatObserver {
+    private void sendMsg() {
+        
+    }
+            
+}
